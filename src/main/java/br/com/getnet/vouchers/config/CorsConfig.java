503cd6aee29ab374f6e8dde48f334983371ee525
evertonsilvagit <everton@evertonsilva.io>
2025-03-20 21:03:51 -0300

    Deploy

--- src/main/java/br/com/getnet/vouchers/config/CorsConfig.java
+++ src/main/java/br/com/getnet/vouchers/config/CorsConfig.java
@@ -1,16 +1,21 @@
 package br.com.getnet.vouchers.config;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.core.env.Environment;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
 public class CorsConfig implements WebMvcConfigurer {
 
+    @Autowired
+    private Environment environment;
+
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")
-                .allowedOriginPatterns("http://192.168.86.*:3000")
+                .allowedOriginPatterns(environment.getProperty("task-backend.allowed-origin-patterns"))
                 .allowedMethods("GET", "POST", "PUT", "DELETE", "HEAD", "OPTIONS")
                 .allowedHeaders("*")
                 .allowCredentials(true);
